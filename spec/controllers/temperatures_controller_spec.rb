57b80a1fe1a41d988ec66f369b156c23c8a0322c
Alex Damian Negru <alex.negru@agilefreaks.com>
2021-11-01 22:29:36 +0200

    [2] Add #index specs

---
+++ spec/controllers/temperatures_controller_spec.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TemperaturesController, type: :controller do
+  describe '#index' do
+    subject(:index) { get :index }
+
+    let(:temperatures) { assigns(:temperatures) }
+
+    it 'fetches temperature objects' do
+      index.then { expect(temperatures).to all be_a Temperature }
+    end
+
+    it 'fetches as many objects as there are types' do
+      index.then { expect(temperatures.count).to eq Temperature::TYPES.count }
+    end
+
+    context 'when the temperatures have been defined' do
+      before do
+        Temperature::TYPES.each_key do |type|
+          cookies[type] = Base64.strict_encode64 attributes_for(:temperature, type).to_json
+        end
+      end
+
+      it 'uses the already defined values' do
+        values = [
+          { type: 'cold', min: 0, max: 18 },
+          { type: 'warm', min: 19, max: 28 },
+          { type: 'hot', min: 29, max: 50 }
+        ]
+
+        index.then { expect(temperatures.map(&:to_h)).to eq values }
+      end
+    end
+
+    context 'when the temperatures have not already been defined' do
+      it 'uses the default values' do
+        values = [
+          { type: 'cold', min: 0, max: 0 },
+          { type: 'warm', min: 0, max: 0 },
+          { type: 'hot', min: 0, max: 0 }
+        ]
+
+        index.then { expect(temperatures.map(&:to_h)).to eq values }
+      end
+    end
+
+    context 'when the cookies have been tampered with' do
+      context 'with invalid values' do
+        before do
+          cookies[:cold] = Base64.strict_encode64 attributes_for(:temperature, :cold).to_json
+          cookies[:warm] = 'invalid'
+          cookies[:hot] = Base64.strict_encode64 attributes_for(:temperature, :hot).to_json
+        end
+
+        it 'deletes the affected cookies' do
+          index.then do
+            expect(cookies[:warm]).to be_blank
+            expect(cookies[:cold]).to be_present
+            expect(cookies[:hot]).to be_present
+          end
+        end
+
+        it 'uses the default values' do
+          values = [
+            { type: 'cold', min: 0, max: 18 },
+            { type: 'warm', min: 0, max: 0 },
+            { type: 'hot', min: 29, max: 50 }
+          ]
+
+          index.then { expect(temperatures.map(&:to_h)).to eq values }
+        end
+      end
+    end
+  end
+end
