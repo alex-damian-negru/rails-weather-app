7fbb2a635cef4c52c7de17b20f06a9e147f8a872
Alex Damian Negru <alex.negru@agilefreaks.com>
2021-11-01 22:28:15 +0200

    [2] Add helper methods

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Model
-  include ActiveModel::Attributes
   include ActiveModel::Model
+
+  def attributes = instance_variables.map(&:to_s).map { _1.delete_prefix('@') }.map(&:to_sym)
+
+  def to_h
+    attributes.reduce({}) { |hash, attribute| hash.merge({ attribute => instance_variable_get("@#{attribute}") }) }
+  end
 end
