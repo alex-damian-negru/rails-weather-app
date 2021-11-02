34b4dd541251e03f273b5f00fa9ec60bd2ed2b68
Alex Damian Negru <alex.negru@agilefreaks.com>
2021-11-02 19:36:58 +0200

    [4] Add WeatherService

---
+++ app/services/weather_service.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class WeatherService
+  ENDPOINT = 'http://api.weatherapi.com/v1'
+
+  class << self
+    def forecast(query)
+      URI(ENDPOINT)
+        .tap { _1.path += '/forecast.json' }
+        .tap { _1.query = { key: ENV['WEATHER_API_KEY'], q: query }.to_query }
+        .then { Net::HTTP.get(_1) }
+        .then { JSON.parse(_1, symbolize_names: true) }
+    end
+  end
+end
---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe WeatherService, :vcr do
+  subject(:service) { described_class }
+
+  describe '.forecast' do
+    subject(:forecast) { service.forecast(query) }
+
+    let(:query) { 'SW1' }
+
+    context 'with an invalid API key' do
+      around do |example|
+        VCR.use_cassette('services/weather_service/invalid_api_key', match_requests_on: [:method]) { example.run }
+      end
+
+      it { is_expected.to eq(error: { code: 2006, message: 'API key is invalid.' }) }
+    end
+
+    context 'with an invalid query' do
+      around do |example|
+        VCR.use_cassette('services/weather_service/invalid_query', match_requests_on: [:method]) { example.run }
+      end
+
+      let(:query) { 'laksjflaslkfdasljgoasjgoiwashogoishg' }
+
+      it { is_expected.to eq(error: { code: 1006, message: 'No matching location found.' }) }
+    end
+
+    context 'with a successful query' do
+      around do |example|
+        VCR.use_cassette('services/weather_service/success', match_requests_on: [:method]) { example.run }
+      end
+
+      it { is_expected.not_to include(:error) }
+    end
+  end
+end
